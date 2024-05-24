97a098098f5b7a40c9237e045a2e219679794fdf
U-ser <john.chanthy@gmail.com>
2024-05-24 23:09:37 +0700

    fix index to use new version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./css/style.css";
-import FileUploader from './FileUpload';
 import App from './App';
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = document.getElementById("root");
+createRoot(root).render(<App />);
