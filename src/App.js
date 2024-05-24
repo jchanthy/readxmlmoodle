0b4462ad70f1877769d8a26d6bd45fd4d26f6382
U-ser <john.chanthy@gmail.com>
2024-05-24 22:56:31 +0700

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import React, { useState } from 'react';
 import FileUploader from './FileUpload';
 
 const App = () => {
-    const [uploadedData, setUploadedData] = useState([]);
-
-    const handleFileUpload = (data) => {
-        setUploadedData(data);
+    const handleFileUpload = () => {
     };
 
     return (
         <>
             <FileUploader onFileUpload={handleFileUpload} />
-            <div>
-                {uploadedData.map((item, index) => (
-                    <div key={index}>
-                        <h3>{item.name}</h3>
-                        <div dangerouslySetInnerHTML={{ __html: item.questiontext }} />
-                        <div>{item.tag}</div>
-                    </div>
-                ))}
-            </div>
         </>
     );
 };
