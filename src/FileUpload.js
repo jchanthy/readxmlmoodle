6fe1889af22d926a0913e4aa5b57ca7f54765c5d
U-ser <john.chanthy@gmail.com>
2024-05-24 23:07:00 +0700

    changed in App file and upload

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,28 +1,6 @@
 import React, { useState } from 'react';
 
 const FileUploader = ({ onFileUpload }) => {
-    const [dragging, setDragging] = useState(false);
-
-    const handleDragOver = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragging(true);
-    };
-
-    const handleDragLeave = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragging(false);
-    };
-
-    const handleDrop = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragging(false);
-        const file = e.dataTransfer.files[0];
-        handleFile(file);
-    };
-
     const handleFileInput = (e) => {
         const file = e.target.files[0];
         handleFile(file);
@@ -45,22 +23,14 @@ const FileUploader = ({ onFileUpload }) => {
 
     return (
         <>
-        <div
-            className={`file-uploader ${dragging ? 'dragging' : ''}`}
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-        >
+
             <input
                 type="file"
                 accept=".xml"
                 onChange={handleFileInput}
                 style={{ display: 'block' }}
             />
-            <label htmlFor="file-input">
-                {dragging ? 'Drop the file here' : 'Drag and drop or click to upload XML file'}
-            </label>
-        </div>
+
             <div className={'result'}>
 
             </div>
