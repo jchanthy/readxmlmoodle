1d867a8f104bd9ac565299ae3c6708bebaaca825
U-ser <john.chanthy@gmail.com>
2024-05-25 00:21:30 +0700

    Update theme using MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import FileUploader from './FileUpload'; // Assuming you have the FileUploader component
+import FileUploader from './FileUpload';
+import LandingPage from "./LandingPage"; // Assuming you have the FileUploader component
 
 const App = () => {
     const handleFileUpload = () => {};
     return (
         <div>
+            <LandingPage />
             <FileUploader onFileUpload={handleFileUpload} />
         </div>
     );
